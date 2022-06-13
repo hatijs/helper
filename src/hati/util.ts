8f1a0bbb3e018a0a96cd0dbd1298d3f650b6096c
Jeong Jun Bak <dev_bjun@meshyarn.io>
2022-06-13 13:17:54 +0900

    #Refac: util.ts

--- src/hati/util.ts
+++ src/hati/util.ts
@@ -2,43 +2,7 @@ import core from '@hatijs/core';
 
 import { constant, position } from '.';
 
-const getPosition = (
-    tjdUT: number,
-    geoLon: number,
-    geoLat: number,
-    hsys: keyof typeof constant.House
-) => {
-    const result = core.node_swe_houses(tjdUT, geoLat, geoLon, hsys);
-
-    if ('error' in result) throw new Error(result.error);
-
-    return result;
-};
-
-const isDiurnal = (
-    tjdUT: number,
-    geoLon: number,
-    geoLat: number,
-    posPoint?: number
-) => {
-    const posHouse = getPosition(tjdUT, geoLon, geoLat, 'WHOLE_SIGN');
-
-    if (!posPoint) {
-        posPoint = position(tjdUT, geoLon, geoLat).getPlanet('SUN').position
-            .longitude.absolute;
-    }
-
-    return !(
-        posPoint > posHouse.ascendant && posHouse.ascendant + 180 >= posPoint
-    );
-};
-
 export const util = {
-    getVersion: () => core.node_swe_version(),
-    getConstellationIndexFromLongitude: (longitude: number) =>
-        Math.floor(longitude / 30) % 12,
-    getConstellationIndexFromConstellationName: (constellationName: string) =>
-        Object.values(constant.Constellation).indexOf(constellationName),
     convertDegreeToDMS: (degree: number) => ({
         degree: Math.floor(degree % 30),
         minute: Math.floor((degree * 60) % 60),
@@ -59,13 +23,49 @@ export const util = {
             },
         },
     }),
+    convertUpperCaseToCapitalize: (str: string) =>
+        str.toLowerCase().replace(/\b[a-z]/g, (char) => char.toUpperCase()),
+    createEnumObject: <T extends string>(o: { [P in T]: P }) => {
+        return o;
+    },
+    getConstellationIndexFromLongitude: (longitude: number) =>
+        Math.floor(longitude / 30) % 12,
+    getConstellationIndexFromConstellationName: (constellationName: string) =>
+        Object.values(constant.Constellation).indexOf(constellationName),
     getDuodecatemorion: (degree: number) => {
         return util.convertDegreeToPosition(
             (degree % 30) * 12 + Math.floor(degree / 30) * 30
         );
     },
-    convertUpperCaseToCapitalize: (str: string) =>
-        str.toLowerCase().replace(/\b[a-z]/g, (char) => char.toUpperCase()),
-    getPosition,
-    isDiurnal,
+    getPosition: (
+        tjdUT: number,
+        geoLon: number,
+        geoLat: number,
+        hsys: keyof typeof constant.House
+    ) => {
+        const result = core.node_swe_houses(tjdUT, geoLat, geoLon, hsys);
+
+        if ('error' in result) throw new Error(result.error);
+
+        return result;
+    },
+    getVersion: () => core.node_swe_version(),
+    isDiurnal: (
+        tjdUT: number,
+        geoLon: number,
+        geoLat: number,
+        posPoint?: number
+    ) => {
+        const posHouse = util.getPosition(tjdUT, geoLon, geoLat, 'WHOLE_SIGN');
+
+        if (!posPoint) {
+            posPoint = position(tjdUT, geoLon, geoLat).getPlanet('SUN').position
+                .longitude.absolute;
+        }
+
+        return !(
+            posPoint > posHouse.ascendant &&
+            posHouse.ascendant + 180 >= posPoint
+        );
+    },
 };
