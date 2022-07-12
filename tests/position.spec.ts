11243cd02a9ec530b877c74fbb1f119f38f25907
Jeong Jun Bak <dev_bjun@meshyarn.io>
2022-07-12 12:03:24 +0900

    #change: test for #fix

--- tests/position.spec.ts
+++ tests/position.spec.ts
@@ -34,14 +34,14 @@ describe('Testing Position', () => {
             });
 
             expect(result).toStrictEqual([
-                [{ name: 'VIRGO' }, 156.3720625401026, 6.372062540102604],
-                [{ name: 'SCORPIO' }, 233.83191934884618, 23.831919348846185],
-                [{ name: undefined }, undefined, NaN],
-                [{ name: undefined }, undefined, NaN],
+                [{ name: 'VIRGO' }, 156.37206254010266, 6.372062540102661],
+                [{ name: 'SCORPIO' }, 233.8319193488462, 23.831919348846213],
+                [{ name: undefined }, NaN, NaN],
+                [{ name: undefined }, NaN, NaN],
                 [{ name: 'VIRGO' }, 154.5076660745534, 4.507666074553413],
-                [{ name: undefined }, undefined, NaN],
-                [{ name: undefined }, undefined, NaN],
-                [{ name: 'CANCER' }, 99.66993930918929, 9.669939309189289],
+                [{ name: undefined }, NaN, NaN],
+                [{ name: undefined }, NaN, NaN],
+                [{ name: 'CANCER' }, 99.66993930918932, 9.669939309189317],
             ]);
         });
     });
@@ -66,12 +66,8 @@ describe('Testing Position', () => {
             });
 
             expect(result).toStrictEqual([
-                [{ name: 'SCORPIO' }, 233.83191934884618, 23.831919348846185],
-                [
-                    { name: 'SAGITTARIUS' },
-                    264.16200636362436,
-                    24.162006363624357,
-                ],
+                [{ name: 'SCORPIO' }, 233.8319193488462, 23.831919348846213],
+                [{ name: 'SAGITTARIUS' }, 264.1620063636244, 24.162006363624414],
                 [{ name: 'CAPRICORN' }, 298.7745623688436, 28.77456236884359],
                 [{ name: 'PISCE' }, 334.5076660745534, 4.507666074553413],
                 [{ name: 'ARIES' }, 6.255373824062872, 6.255373824062872],
@@ -80,7 +76,7 @@ describe('Testing Position', () => {
                 [{ name: 'GEMINI' }, 84.16200636362436, 24.162006363624357],
                 [{ name: 'CANCER' }, 118.77456236884359, 28.77456236884359],
                 [{ name: 'VIRGO' }, 154.5076660745534, 4.507666074553413],
-                [{ name: 'LIBRA' }, 186.2553738240629, 6.2553738240629],
+                [{ name: 'LIBRA' }, 186.25537382406287, 6.255373824062872],
                 [{ name: 'SCORPIO' }, 212.2302153244375, 2.230215324437495],
             ]);
         });
@@ -98,13 +94,13 @@ describe('Testing Position', () => {
                         absolute: 243.39436188691664,
                         relative: 3.3943618869166414,
                     },
-                    latitude: -0.00007563938614282484,
+                    latitude: -0.000075639386139611,
                     rectAscension: 241.36853615800743,
-                    declination: -20.832643413219806,
+                    declination: -20.83264341321981,
                 },
                 speed: {
-                    longitude: 1.0116833781933507,
-                    latitude: 0.000010875746705972084,
+                    longitude: 1.0116833781577443,
+                    latitude: 0.000010875754566425983,
                 },
             });
         });
@@ -118,8 +114,8 @@ describe('Testing Position', () => {
                 constellation: { name: 'AQUARIUS' },
                 position: {
                     longitude: {
-                        absolute: 328.5666862135374,
-                        relative: 28.566686213537423,
+                        absolute: 328.5666862135388,
+                        relative: 28.566686213538787,
                     },
                 },
             });
