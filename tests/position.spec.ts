2edbb5c442acce48e4d93f763545769c5b5e4f88
Jeong Jun Bak <dev_bjun@meshyarn.io>
2022-11-17 13:09:03 +0900

    #change: .

--- tests/position.spec.ts
+++ tests/position.spec.ts
@@ -8,7 +8,7 @@ beforeAll(() => {
 describe('Testing Position', () => {
     describe('getHouse', () => {
         test('PORPHYRY', () => {
-            const houses = hati.position.getHouse('PORPHYRY');
+            const houses = hati.position.getHouse('PORPHYRY', true);
             const keys = Hati.util.createEnumObject<
                 Parameters<typeof houses>[0]
             >({
@@ -22,16 +22,27 @@ describe('Testing Position', () => {
                 VERTEX: 'VERTEX',
             });
 
-            const result = Object.keys(keys).map((house) => {
-                const { constellation, position } = houses(
+            const result = [];
+            const result_dodecatemoria = [];
+            for (const house of Object.keys(keys)) {
+                const data = houses(
                     <keyof typeof houses>house
                 );
-                return [
-                    constellation,
-                    position.longitude.absolute,
-                    position.longitude.relative,
-                ];
-            });
+
+                result.push([
+                    data.constellation,
+                    data.position.longitude.absolute,
+                    data.position.longitude.relative,
+                ]);
+
+                if ('dodecatemoria' in data) {
+                    result_dodecatemoria.push([
+                        data.dodecatemoria.constellation,
+                        data.dodecatemoria.position.longitude.absolute,
+                        data.dodecatemoria.position.longitude.relative,
+                    ]);
+                }
+            }
 
             expect(result).toStrictEqual([
                 [{ name: 'VIRGO' }, 156.37206254010266, 6.372062540102661],
@@ -43,27 +54,48 @@ describe('Testing Position', () => {
                 [{ name: undefined }, NaN, NaN],
                 [{ name: 'CANCER' }, 99.66993930918932, 9.669939309189317],
             ]);
+
+            expect(result_dodecatemoria).toStrictEqual([
+                [{ name: 'SCORPIO' }, 226.46475048123193, 16.46475048123193],
+                [{ name: 'LEO' }, 135.98303218615456, 15.98303218615456],
+                [{ name: undefined }, NaN, NaN],
+                [{ name: undefined }, NaN, NaN],
+                [{ name: 'LIBRA' }, 204.09199289464095, 24.091992894640953],
+                [{ name: undefined }, NaN, NaN],
+                [{ name: undefined }, NaN, NaN],
+                [{ name: 'LIBRA' }, 206.0392717102718, 26.039271710271805]
+            ]);
         });
     });
 
     describe('getHouses', () => {
         test('PORPHYRY', () => {
-            const houses = hati.position.getHouses('PORPHYRY');
+            const houses = hati.position.getHouses('PORPHYRY', true);
+            const house_array = Array(12)
+                .fill(() => 0)
+                .map((_, i) => i);
 
-            const result = Object.keys(
-                Array(12)
-                    .fill(() => 0)
-                    .map((_, i) => i)
-            ).map((house) => {
-                const { constellation, position } = houses(
+            const result = [];
+            const result_dodecatemoria = [];
+            for (const house of Object.keys(house_array)) {
+                const data = houses(
                     <keyof typeof houses>house
                 );
-                return [
-                    constellation,
-                    position.longitude.absolute,
-                    position.longitude.relative,
-                ];
-            });
+
+                result.push([
+                    data.constellation,
+                    data.position.longitude.absolute,
+                    data.position.longitude.relative,
+                ]);
+
+                if ('dodecatemoria' in data) {
+                    result_dodecatemoria.push([
+                        data.dodecatemoria.constellation,
+                        data.dodecatemoria.position.longitude.absolute,
+                        data.dodecatemoria.position.longitude.relative,
+                    ]);
+                }
+            }
 
             expect(result).toStrictEqual([
                 [{ name: 'SCORPIO' }, 233.8319193488462, 23.831919348846213],
@@ -79,12 +111,27 @@ describe('Testing Position', () => {
                 [{ name: 'LIBRA' }, 186.25537382406287, 6.255373824062872],
                 [{ name: 'SCORPIO' }, 212.2302153244375, 2.230215324437495],
             ]);
+
+            expect(result_dodecatemoria).toStrictEqual([
+                [{ name: 'LEO' }, 135.98303218615456, 15.98303218615456],
+                [{ name: 'VIRGO' }, 169.94407636349297, 19.944076363492968],
+                [{ name: 'SAGITTARIUS' }, 255.29474842612308, 15.294748426123078],
+                [{ name: 'ARIES' }, 24.091992894640953, 24.091992894640953],
+                [{ name: 'GEMINI' }, 75.06448588875446, 15.064485888754461],
+                [{ name: 'TAURUS' }, 56.76258389324994, 26.76258389324994],
+                [{ name: 'AQUARIUS' }, 315.98303218615456, 15.98303218615456],
+                [{ name: 'PISCE' }, 349.9440763634923, 19.944076363492286],
+                [{ name: 'GEMINI' }, 75.29474842612308, 15.294748426123078],
+                [{ name: 'LIBRA' }, 204.09199289464095, 24.091992894640953],
+                [{ name: 'SAGITTARIUS' }, 255.06448588875446, 15.064485888754461],
+                [{ name: 'SCORPIO' }, 236.76258389324994, 26.76258389324994]
+            ]);
         });
     });
 
     describe('getPlanet', () => {
         test('SUN', () => {
-            const sun = hati.position.getPlanet('SUN');
+            const sun = hati.position.getPlanet('SUN', true);
 
             expect(sun).toStrictEqual({
                 name: 'SUN',
@@ -107,13 +154,22 @@ describe('Testing Position', () => {
                         longitude: 1.0265107557767934
                     }
                 },
+                dodecatemoria: {
+                    constellation: { name: 'CAPRICORN' },
+                    position: {
+                        longitude: {
+                            absolute: 280.7323426429997,
+                            relative: 10.732342642999697,
+                        },
+                    },
+                },
             });
         });
     });
 
     describe('getLot', () => {
         test('FORTUNE', () => {
-            const fortune = hati.position.getLot('FORTUNE');
+            const fortune = hati.position.getLot('FORTUNE', true);
 
             expect(fortune).toStrictEqual({
                 constellation: { name: 'AQUARIUS' },
@@ -121,6 +177,15 @@ describe('Testing Position', () => {
                     longitude: {
                         absolute: 328.5666862135374,
                         relative: 28.566686213537423,
+                    },
+                },
+                dodecatemoria: {
+                    constellation: { name: 'CAPRICORN' },
+                    position: {
+                        longitude: {
+                            absolute: 282.8002345624491,
+                            relative: 12.800234562449077,
+                        },
                     },
                 },
             });
